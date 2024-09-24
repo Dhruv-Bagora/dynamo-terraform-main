2a445336d6951b41a3c816e40ccbcea89b22f7e4
Dhruv-Bagora <https://github.com/Dhruv-Bagora>
2024-09-24 18:06:24 +0530

    Workflow updated and ec2 resource added in the infra

---
+++ ec2.tf
@@ -0,0 +1,38 @@
+# Data source to get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Data source to get the default subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Fetch the latest Ubuntu 22.04 LTS AMI using the AWS SSM Parameter Store
+data "aws_ssm_parameter" "ubuntu_ami" {
+  name = "/aws/service/canonical/ubuntu/eks-pro/22.04/1.29/stable/20240410/amd64/hvm/ebs-gp2/ami-id"
+}
+
+# Create an EC2 instance in the default VPC and subnet
+resource "aws_instance" "example1" {
+  ami           = data.aws_ssm_parameter.ubuntu_ami.value
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnets.default.ids[0] # Use the first subnet from the default VPC
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+resource "aws_instance" "example2" {
+  ami           = data.aws_ssm_parameter.ubuntu_ami.value
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnets.default.ids[0] # Use the first subnet from the default VPC
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
